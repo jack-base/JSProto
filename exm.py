f926a29b1721e4b966c6bfab341e347dcce8806e
yjk114514 <591521420@qq.com>
2025-07-08 04:20:01 +0800

    refactor(jsproto): rename project and update structure
    - Rename project from jspy to JSProto
    - Update module and file names accordingly
    - Refactor jsproto package structure
    - Add new methods to var class
    - Update example script
    - Modify project configuration files

--- exm.py
+++ exm.py
@@ -1,5 +1,4 @@
 from jsproto import *
-
 (var(['apple', 'banana', 'pear'])
  .forEach(function(lambda s: 'long' if len(s) > 5 else 'short')
           .then(print)))
@@ -17,9 +16,22 @@ from jsproto import *
 }).contact.email
  .then(print))
 
+
+my_dict = var({
+    "name": "John",
+    "age": 30,
+    "contact": {
+        "email": "john@example.com",
+        "phone": "1234567890"
+    },
+    "hobbies": ["reading", "swimming", "traveling"],
+    "is_student": False
+})
+my_dict.contact = var({})
+print(my_dict.toString())
 # 输出
 # short
 # long
 # short
 # john@example.com
-
+# {}
